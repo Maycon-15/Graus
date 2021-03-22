82895b25f9296443684437a04ad3e692d979df7e
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-21 22:50:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Graus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            double C,F;
+
+            Console.Write("Digite a temperatura (em °F): ");
+            entrada = Console.ReadLine();
+            F= Convert.ToDouble(entrada);
+
+            C = (F-32)/1.8;
+
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.WriteLine("\n---Equivalencia---");
+            Console.ResetColor();
+            Console.WriteLine($"Essa temperatura em °C fica: {C}");
+            
+        }
+    }
+}
